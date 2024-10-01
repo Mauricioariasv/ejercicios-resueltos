94a7ca8c96063cf2b5e2ed23d66a8dbbff277721
Mauricio Arias <mauricioariasalfonzo@gmail.com>
2024-10-01 19:46:55 -0400

    Ejercicio 50

---
+++ ejercicio_50.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $conexionStr = 'mysql:host=localhost;dbname=usm;charset=utf8';
+ $conexion = new PDO($conexionStr, "root", "");
+ $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ $resultado = $conexion->prepare("SELECT estudiantes.*, cursos.nombre as curso FROM estudiantes INNER JOIN cursos ON estudiantes.curso_id = cursos.curso_id");
+ $resultado->execute();
+ $estudiantes = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($estudiantes as $estudiante)
+{
+ echo "Codigo:".$estudiante['nombre']."<br>";
+ echo "Nombre:".$estudiante['correo']."<br>";
+ echo "Curso:".$estudiante['curso']."<br>";
+ echo "<hr>";
+}
