66300c6d597202767d5cba2e46cfeca6321250b3
Mauricio Arias <mauricioariasalfonzo@gmail.com>
2024-10-01 21:10:18 -0400

    Ejercicio 40 con mysql

--- ejercicio_40.php
+++ ejercicio_40.php
@@ -1,14 +1,10 @@
 <h1>Mostrar nombres de estudiantes de la BD </h1>
 <?php
- $conexionStr = 'mysql:host=localhost;dbname=usm;charset=utf8';
- $conexion = new PDO($conexionStr, "root", "");
- $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $conex = mysqli_connect("localhost", "root", "", "usm");
+ $result = mysqli_query($conex, "SELECT * FROM estudiantes");
 
- $resultado = $conexion->prepare("SELECT * FROM estudiantes");
- $resultado->execute();
- $students = $resultado->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($students as $student) {
-    echo $student["nombre"] . "<br>";
-}
-?> 
+ if($result) {
+    while ($estudiante = mysqli_fetch_array($result)) {
+        echo $estudiante["nombre"] . "<br>";
+    }
+ }
