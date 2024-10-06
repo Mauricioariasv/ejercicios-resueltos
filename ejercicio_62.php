a38bc7b94e0bdf59e62166e6e919d7fae6ed9960
Mauricio Arias <mauricioariasalfonzo@gmail.com>
2024-10-06 16:48:03 -0400

    Ejercicios

---
+++ ejercicio_62.php
@@ -0,0 +1,6 @@
+<?php 
+require_once("ejercicio_62_funciones.php"); 
+cabeceraPagina("Titulo principal de la página"); 
+echo "<br><center>Este es el cuerpo de la página<br></center>"; 
+piePagina("Pie de la página con el archivo externo"); 
+?> 
---
+++ ejercicio_62_funciones.php
@@ -0,0 +1,15 @@
+<?php 
+
+function cabeceraPagina($tit) 
+{ 
+ echo "<table width=\"100%\"><tr><td bgcolor=\"#45CFDD\" 
+ align=\"center\">$tit</td></tr></table>"; 
+}
+
+function piePagina($tit) 
+{ 
+ echo "<table width=\"25%\" align=\"center\"><tr><td 
+ bgcolor=\"#A7EDE7\">$tit</td></tr></table>"; 
+} 
+
+?>
